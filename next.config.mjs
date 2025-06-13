e2ac67cd43015f124f9fd54498a8cfed542e53fb
abhinav <abhinavisgreat18@gmail.com>
2025-06-13 14:43:41 +0530

    updated nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode:false,
+  output: 'standalone', 
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true, 
+  },
 };
 
 export default nextConfig;
